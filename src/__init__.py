f2f5f98577a3a8fe7cfbcf7401d15689f85fb19b
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-09-15 01:19:52 -0900

    connected to db

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_sapn(app: FastAPI):
+    print("Server is starting...")
+    await init_db()
+    yield
+    print("Server has been stopped")
 
 version = 'v1'
 
@@ -7,6 +16,7 @@ app = FastAPI(
     title = "Books",
     description = 'A REST_API for book reviews.',
     version = version,
+    lifespan=life_sapn
 )
 
 
